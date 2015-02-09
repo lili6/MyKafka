fe7bfe4df3699e68173720f0a3d012d47a4e0946
lili6 <34793278@qq.com>
2015-02-09 18:36:27 +0800

    avro sample

---
+++ src/main/java/lili/avro/sample/GenericMain.java
@@ -0,0 +1,59 @@
+package lili.avro.sample;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.avro.Schema;
+import org.apache.avro.Schema.Parser;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+/**
+ * Created by liguofang on 2015/2/9.
+ * 构造为通用的对象
+ */
+public class GenericMain {
+	public static void main(String[] args) throws IOException {
+		File f = new File(GenericMain.class.getResource("/avro/user.avsc").getFile());
+//		Schema schema = new Parser().parse(new File("/avro/user.avsc"));
+		Schema schema = new Parser().parse(f);
+
+
+		GenericRecord user1 = new GenericData.Record(schema);
+		user1.put("name", "Alyssa");
+		user1.put("favorite_number", 256);
+		// Leave favorite color null
+
+		GenericRecord user2 = new GenericData.Record(schema);
+		user2.put("name", "Ben");
+		user2.put("favorite_number", 7);
+		user2.put("favorite_color", "red");
+
+		// Serialize user1 and user2 to disk将对象序列化到磁盘上
+		File file = new File("users.avro");
+		DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<GenericRecord>(schema);
+		DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<GenericRecord>(datumWriter);
+		dataFileWriter.create(schema, file);
+		dataFileWriter.append(user1);
+		dataFileWriter.append(user2);
+		dataFileWriter.close();
+
+		// Deserialize users from disk 从磁盘反序列化文件 并生成对象
+		DatumReader<GenericRecord> datumReader = new GenericDatumReader<GenericRecord>(schema);
+		DataFileReader<GenericRecord> dataFileReader = new DataFileReader<GenericRecord>(file, datumReader);
+		GenericRecord user = null;
+		while (dataFileReader.hasNext()) {
+			// Reuse user object by passing it to next(). This saves us from
+			// allocating and garbage collecting many objects for files with
+			// many items.
+			user = dataFileReader.next(user);
+			System.out.println(user);
+		}
+
+	}
+}
