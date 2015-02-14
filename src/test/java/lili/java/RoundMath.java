cfb680704f5013a2eb84a6e629211660d47eb079
lili6 <34793278@qq.com>
2015-02-14 22:41:50 +0800

    roundmath

---
+++ src/test/java/lili/java/RoundMath.java
@@ -0,0 +1,14 @@
+package lili.java;
+
+/**
+ * Created by lili on 15/2/13.
+ */
+public class RoundMath {
+
+    public static  void main(String[] args) {
+//        double d = 1256.22d;
+        double d = 1246.22d;
+        d= d/100;
+        System.out.println(Math.round(d) *100);
+    }
+}
